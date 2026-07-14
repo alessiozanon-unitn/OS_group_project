1c0038e36c1fe4356e3ea865b8c527cb130c6665
Alessio <alessio.zanon@unitn.it>
2026-07-14 12:49:53 +0200

    Another modification to kitchen.h

--- code/src/kitchen.h
+++ code/src/kitchen.h
@@ -11,6 +11,7 @@ typedef struct ResourceUnit{
 typedef struct Resource {
   ResourceUnit unit;
   int clean_time;
+  int initialResourceCount;
   sem_t clean; //Initialized to resource count
   sem_t dirty; //Initialized at 0, signaled when the reource is used
   int* dirtyResourceCounters; //Of size resource count, initialized to full zeroes and updated individually
