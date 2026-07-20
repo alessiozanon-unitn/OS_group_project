2f9ce18351ec0ed35c63b08d9838f74701d15dba
Lorenzo <medurilorenzo@proton.me>
2026-07-20 14:19:49 +0200

    Added testing library (Unity) + make test

---
+++ code/tests/test.c
@@ -0,0 +1,19 @@
+#include "unity.h"
+#include <stdatomic.h>
+#include <stdio.h>
+
+atomic_int score;
+double gameSpeed;
+
+void setUp(void){
+
+}
+void tearDown(void){
+
+}
+
+int main(){
+  printf("ciao");
+
+  return 0;
+}
